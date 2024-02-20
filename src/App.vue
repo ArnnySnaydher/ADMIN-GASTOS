3b7ea051be53e9012ed3f62918f52d480de2d930
Aryans <quispeloayza18@hotmail.com>
2024-02-20 17:10:04 -0500

    Css Global

--- src/App.vue
+++ src/App.vue
@@ -11,8 +11,48 @@
 </div>
 </template>
 
-<style scoped>
- h1,h2{
-  color: rebeccapurple;
- }
+<style>
+  :root{
+    --azul: #3b82f6;
+    --blanco: #fff;
+    --gris-claro: #f5f5f5;
+    --gris:#94a3b8;
+    --gris-oscuro:#64748b;
+    --negro:#000;
+  }
+
+  html{
+    font-size: 62.5%;
+    box-sizing: border-box;
+  }
+  *,
+  *:before,
+  *::after{
+    box-sizing:inherit
+  }
+
+  body{
+    font-size: 1.6rem;
+    font-family: "Lato", sans-serif;
+    background-color: var(--gris-claro);
+
+  }
+
+  h1{
+    font-size: 4rem;
+
+  }
+  h2{
+    font-size: 3rem;
+  }
+
+  header{
+    background-color: var(--azul);
+  }
+  header h1{
+    padding: 3rem 0;
+    margin: 0;
+    color: var(--blanco);
+    text-align: center;
+  }
 </style>
