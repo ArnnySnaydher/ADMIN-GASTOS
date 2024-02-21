1e6af343850f4a3124efb2ba8b5fc7c9f9568773
Aryans <quispeloayza18@hotmail.com>
2024-02-21 17:54:37 -0500

    Estilo al control del presupuesto

--- src/components/ControlPresupuesto.vue
+++ src/components/ControlPresupuesto.vue
@@ -1,6 +1,6 @@
 
 <script setup>
-    import imagen from  '../assets/img/grafico.jpg'
+import imagen from '../assets/img/grafico.jpg'
 </script>
 <template>
     <div class="dos-columnas">
@@ -27,5 +27,62 @@
 
 
 <style scoped>
+.dos-columnas {
+    display: flex;
+    flex-direction: column;
+}
 
+.dos-columnas>:first-child {
+    margin-bottom: 3rem;
+}
+
+.contenedor-presupuesto {
+    width: 100%;
+}
+
+.contenedor-presupuesto p {
+    font-size: 2.4rem;
+    text-align: center;
+    color: var(--gris-oscuro);
+}
+
+.contenedor-presupuesto span {
+    font-weight: 900;
+    color: var(--azul);
+}
+
+.reset-app{
+    background-color: #db2777;
+    border: none;
+    padding: 1rem;
+    width: 100%;
+    color: var(--blanco);
+    font-weight: 900;
+    text-transform: uppercase;
+    border-radius: 1rem;
+    transition-property: background-color;
+    transition-duration: 300ms;
+}
+
+.reset-app:hover{
+    cursor: pointer;
+    background-color: #c11d67;
+}
+
+@media (min-width: 768px) {
+    .dos-columnas {
+        flex-direction: row;
+        gap: 4rem;
+        align-items: center;
+    }
+
+    .dos-columnas>:first-child {
+        margin-bottom: 0;
+    }
+
+    .contenedor-presupuesto {
+        font-size: 2.4rem;
+        text-align: left;
+    }
+}
 </style>
